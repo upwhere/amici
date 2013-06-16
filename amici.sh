fd327d92d568b1e57d3fdfe3da1e37dca566d49d
up here <iam@upwhere.me>
2013-06-16 17:08:16 +0000

    hardened functions against unexpected cases

--- amici.sh
+++ amici.sh
@@ -28,17 +28,17 @@
 
 #produce a sane environment.
 unalias -a
-PATH="/sbin:/bin:/usr/sbin:/usr/bin"
+readonly PATH="/sbin:/bin:/usr/sbin:/usr/bin"
 cd /
 hash -r
 ulimit -H -c 0
-IFS="
+readonly IFS="
  "
 
 set -e
 
 # wikipedia-listed domains contributing to PRISM 2013-06-15
-prism="google.com
+readonly prism="google.com
 microsoft.com
 apple.com
 skype.com
@@ -52,103 +52,155 @@ aol.com"
 debug="false"
 dryrun="false"
 
-database="dig"
-query="+short -t"
-spfquery="$query TXT"
+readonly database="dig"
+readonly query="+short -t"
+readonly spfquery="$query TXT"
+readonly aquery="$query A"
 
 #lets them know this host is prohibited
-ip4injob="--append INPUT --jump REJECT --reject-with icmp-host-prohibited"
+readonly ip4injob="--jump REJECT --reject-with icmp-host-prohibited"
 #lets you know their network is prohibited.
-ip4outjob="--append OUTPUT --jump REJECT --reject-with icmp-host-prohibited"
-ip6job="--jump REJECT --reject-with icmp6-adm-prohibited"
+readonly ip4outjob="--jump REJECT --reject-with icmp-net-prohibited"
+readonly ip6job="--jump REJECT --reject-with icmp6-adm-prohibited"
 
 echoerr() { echo "$@" >&2; }
+echodebug() { $debug && echo "$@" ; true; }
 
+# check if all the commands we need are available
 command -v grep >/dev/null || { echoerr "Dependency not met: program: grep"; exit 1; }
 command -v sed >/dev/null || { echoerr "Dependency not met: program: sed"; exit 1; }
 command -v $database >/dev/null || { echoerr "Dependency not met: program: $database"; exit 1; }
 command -v iptables >/dev/null || { echoerr "Dependency not met: program: iptables"; exit 1; }
 command -v ip6tables >/dev/null || { echoerr "Dependency not met: program: ip6tables"; exit 1; }
 
+## block IPv4 addresses 
 function block4
 {
-	$dryrun||iptables $ip4injob --source $@
-	$dryrun||iptables $ip4outjob --destination $@
-	$debug&&echo "		ip4:$@"
+	for address in "$@"; do
+		#make sure it does not exist yet
+		iptables --check INPUT $ip4injob --source $address 2>/dev/null ||
+		{
+			#block if no dry-run
+			$dryrun||iptables --append INPUT $ip4injob --source $address
+		}
+		#make sure it does not exist yet
+		iptables --check OUTPUT $ip4outjob --destination $address 2>/dev/null ||
+		{
+			#block if no dry-run
+			$dryrun||iptables --append OUTPUT $ip4outjob --destination $address
+		}
+		echodebug "		ip4:$address"
+	done
 }
 
+## block IPv6 addresses
 function block6
 {
-	$dryrun||ip6tables $ip6job --append INPUT --source $@
-	$dryrun||ip6tables $ip6job --append OUTPUT --destination $@
-	$debug&&echo "		ip6:$@"
+	for address in "$@"; do
+		# make sure this rule does not exist yet
+		ip6tables --check INPUT $ip6job --source $@ 2>/dev/null ||
+		{
+			#if not a dry-run, add it
+			$dryrun||ip6tables $ip6job --append INPUT --source $@
+		}
+		# make sure this rule does not exist yet
+		ip6tables --check OUTPUT $ip6job --destination $@ 2>/dev/null ||
+		{
+			# and if it's not a dry-run, add it.
+			$dryrun||ip6tables $ip6job --append OUTPUT --destination $@
+		}
+		echodebug "		ip6:$@"||true
+	done
 }
 
+## block all the DNS addresses we can identify
 function blockdomain
 {
-	$debug&&echo "	$@"
-	block4 $@
-	block6 $@
+	for bdomain in "$@";do
+		echodebug "	$bdomain"
+		block4 $($database $aquery $bdomain)
+		#block6 $@
+	done
 }
 
+## search for and block everything in the SPF records of the passed domains
 function blockspf
 {
+	## make sure domain is retained during recursion
+	local domain
 	for domain in "$@"; do
-		$debug&&echo $domain
-		### FIXME: ###
-		#
-		# a single text DNS
-		# record (either TXT or SPF RR types) can be composed of more than one
-		# string.  If a published record contains multiple strings, then the
-		# record MUST be treated as if those strings are concatenated together
-		# without adding spaces.
-		#
-		spf=$($database $spfquery $domain|grep --only-matching --perl-regexp "(\").*\1"|sed '{s/\"//g}'|grep v=spf)
-		# clean up modifiers
-		for entry in $spf; do
-			entry=${entry#[-~\?+]}
-			case $entry in
-				##new domains to search##
-				include:*|ptr:*|a:*)
-					blockspf ${entry#*:}
-				;;
-				exists:*)
-					#unparsable
-				;;
-				redirect=*)
-					blockspf ${entry#redirect=}
-				;;
-				##ranges to block##
-				ip4:*)
-					block4 ${entry#ip4:}
-				;;
-				ip6:*)
-					block6 ${entry#ip6:}
-				;;
-				##spf record specifics##
-				a|mx|ptr|all)
-					#covered by blockdomain $domain, below
-				;;
-				exp=*)
-					#discard explanations, often pattern expanded and thus unparsable.
-				;;
+		echodebug $domain
+
+		local txtrecords="$($database $spfquery $domain)
+"		#while there are TXT records left
+		while [ -n "$txtrecords" ];do
+			#grab one record
+			local record=${txtrecords%%
+*}
+			#remove the record
+			txtrecords=${txtrecords##*
+}
+			#strip the quotes
+			record=${record//\"/}
+			case $record in
 				v=spf*)
-					version=${entry#v=}
-					if [ $version != "spf1" ];then
-						echoerr "careful: spf version unknown: $version"
-						# we don't care about integrity, we just want to find all associated domains, continue anyway
-					fi
+					# each space-separated word...
+					for entry in $record; do
+						# clean up SPF modifiers
+						entry=${entry#[-~\?+]}
+						case $entry in
+							##new domains to search##
+							include:*|ptr:*|a:*)
+								blockspf ${entry#*:}
+							;;
+							exists:*)
+								#unparsable
+							;;
+							redirect=*)
+								blockspf ${entry#redirect=}
+							;;
+							##ranges to block##
+							ip4:*)
+								block4 ${entry#ip4:}
+							;;
+							ip6:*)
+								block6 ${entry#ip6:}
+							;;
+							##spf record specifics##
+							a|mx|ptr|all)
+								#covered by blockdomain $domain, below
+							;;
+							exp=*)
+								#discard explanations, often pattern expanded and thus unparsable.
+							;;
+							v=spf*)
+								version=${entry#v=}
+								if [ $version != "spf1" ];then
+									echoerr "careful: SPF version unknown: $version"
+									# we don't care about integrity, we just want to find all associated domains, continue anyway
+								fi
+							;;
+							*)
+								echoerr "$domain has unknown entry type: $entry"
+								$debug&&echoerr "	record:$spf"
+							;;
+						esac
+					done
+				;;
+				## if there is no TXT record in this domain, just continue
+				"")
 				;;
 				*)
-					echoerr "$domain has unknown entry type: $entry"
-					$debug&&echoerr "	record:$spf"
+					echodebug "TXT unidentified: $record"
 				;;
 			esac
 		done
+		## now that the SPF has been parsed and blocked, block everything else on this domain.
 		blockdomain $domain
 	done
 }
 
+## parse command-line arguments
 for argument in "$@";do
 	case $argument in
 		-v|--debug)
@@ -172,9 +224,11 @@ $argument"
 	esac
 done
 
+# nothing to block?
 if [ -z "$blocks" ];then
 	echoerr "Usage: $0 [--debug,--prism,--dryrun] [domain [domain [...]]]"
 	exit 1
 fi
 
+#start the recursion
 blockspf $blocks
